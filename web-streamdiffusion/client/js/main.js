9d0a46dcde3eeaa900975e55af8870dd39650c29
jacques <janneslambrecht@hotmail.com>
2025-05-15 18:16:30 +0200

    update web-stream

---
+++ web-streamdiffusion/client/js/main.js
@@ -0,0 +1,64 @@
+
+const messageInput = document.getElementById('messageInput');
+const sendButton = document.getElementById('sendButton');
+const img = document.getElementById('frame');
+
+const currentIP = '192.168.129.31';
+const currentPort = '5002';
+const currentURL = `ws://${currentIP}:${currentPort}`;
+const socket = new WebSocket(currentURL);
+
+socket.binaryType = 'arraybuffer';
+
+socket.onopen = () => {
+  console.log('WebSocket connected.');
+};
+
+socket.onmessage = (event) => {
+  const buffer = event.data;
+  const text = new TextDecoder().decode(buffer);
+
+  try {
+    const json = JSON.parse(text);
+    if (json.type === 'frame' && json.data) {
+      img.src = 'data:image/jpeg;base64,' + json.data;
+    } else {
+      console.warn('Unexpected message:', json);
+    }
+    if (json.type === 'updated-prompt') {
+      document.getElementById('currentPrompt').textContent = json.data;
+    }
+
+  } catch (e) {
+    console.error('Error parsing frame:', e);
+  }
+};
+
+socket.onerror = (err) => {
+  console.error('WebSocket error:', err);
+};
+
+socket.onclose = () => {
+  console.warn('WebSocket connection closed');
+};
+
+
+sendButton.addEventListener('click', () => {
+  const message = messageInput.value;
+  if (message && socket.readyState === WebSocket.OPEN) {
+    const json = JSON.stringify({
+      type: 'updated-prompt',
+      message: message
+    });
+    document.getElementById('currentPrompt').textContent = message;
+    console.log('Sending message:', json);
+    socket.send(json);
+    messageInput.value = '';
+  }
+});
+
+messageInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    sendButton.click();
+  }
+});
---
+++ web-streamdiffusion/server/src/main.js
@@ -0,0 +1,36 @@
+import { WebSocketServer, WebSocket } from 'ws';
+
+const portWebSocket = 5002;
+
+// Create a WebSocket server
+const wss = new WebSocketServer({ port: portWebSocket });
+
+wss.on('connection', (ws) => {
+    console.log('A user connected via WebSocket');
+
+    ws.on('message', (message) => {
+        console.log('Received message via WebSocket:', message);
+        const data = JSON.parse(message.toString());
+
+        if (data.type === 'updated-prompt') {
+            message = JSON.stringify({
+                type: 'updated-prompt',
+                data: data.message
+            });
+        }
+        // Broadcast to all other connected clients
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+
+        // console.log('Broadcasted message to all WebSocket clients:', message);
+    });
+
+    ws.on('close', () => {
+        console.log('A user disconnected from WebSocket');
+    });
+});
+
+console.log(`Pure WebSocket server running on port ${portWebSocket}`);
